e0b870bc953b80c8293f2328a2269a7c0ba38e46
leopauld ndenko <lndenko18@gmail.com>
2022-12-10 08:35:12 +0400

    files updated

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
 const keys= document.querySelectorAll('.key');
-let numbers;
+const result=document.querySelector('.result');
+const equal=document.querySelector('.equal');
+const reset=document.querySelector('#reset');
+const del=document.querySelector('#delete');
+const themeToggle1=document.querySelector('.btn1');
+const themeToggle2=document.querySelector('.btn2');
+const themeToggle3=document.querySelector('.btn3');
+const container =document.querySelector('.container')
+const span =document.querySelector('.span')
+
+
+themeToggle1.addEventListener('click',()=>{
+  // document.body.classList.add('theme1')
+  // document.body.classList.remove('theme2')
+  // document.body.classList.remove('theme3')
+  
+  container.classList.add('theme1')
+  container.classList.remove('theme2')
+  container.classList.remove('theme3')
+  span.classList.add('theme1')
+  span.classList.remove('theme2')
+  span.classList.remove('theme3')
+  
+})
+themeToggle2.addEventListener('click',()=>{
+  // document.body.classList.add('theme2')
+  // document.body.classList.remove('theme1')
+  // document.body.classList.remove('theme3')
+  container.classList.add('theme2')
+  container.classList.remove('theme1')
+  container.classList.remove('theme3')
+  span.classList.add('theme2')
+  span.classList.remove('theme1')
+  span.classList.remove('theme3')
+})
+themeToggle3.addEventListener('click',()=>{
+  // document.body.classList.add('theme3')
+  // document.body.classList.remove('theme1')
+  // document.body.classList.remove('theme2')
+  document.body.style.color='red'
+  container.classList.add('theme3')
+  container.classList.remove('theme1')
+  container.classList.remove('theme2')
+  span.classList.add('theme3')
+  span.classList.remove('theme1')
+  span.classList.remove('theme2')
+})
 
 keys.forEach(key => {
-  let result=document.querySelector('#result');
   
   key.addEventListener('click',(e)=>{
-    numbers=key.innerText('34')
-    console.log(key.innerText)
-    result.innerHTML=numbers
+    result.textContent += e.target.id; 
   })
 
 });
+
+ equal.addEventListener('click',()=>{
+  result.textContent = eval(result.textContent)
+ })
+
+ reset.addEventListener('click',()=>{
+  result.textContent= ""
+ })
+
+ del.addEventListener('click',()=>{
+  if(result.textContent==''){
+    result.textContent= ''
+  }else{
+    result.textContent=result.textContent.slice(0,-1);
+  }
+ })
