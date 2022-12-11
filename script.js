2dcadf5e1b27890968939e67ef58b95c2f35a3d8
leopauld ndenko <lndenko18@gmail.com>
2022-12-11 16:42:57 +0400

    files updated

--- script.js
+++ script.js
@@ -8,43 +8,58 @@ const themeToggle2=document.querySelector('.btn2');
 const themeToggle3=document.querySelector('.btn3');
 const container =document.querySelector('.container')
 const span =document.querySelector('.span')
+const btnColor1 =document.querySelector('.btn-color1')
+const btnColor2 =document.querySelector('.btn-color2')
+const btnColor3 =document.querySelector('.btn-color3')
 
-
+ 
 themeToggle1.addEventListener('click',()=>{
   document.body.classList.add('theme1')
   document.body.classList.remove('theme2')
   document.body.classList.remove('theme3')
   
-  container.classList.add('theme1')
-  container.classList.remove('theme2')
-  container.classList.remove('theme3')
-  span.classList.add('theme1')
-  span.classList.remove('theme2')
-  span.classList.remove('theme3')
+  // container.classList.add('theme1')
+  // container.classList.remove('theme2')
+  // container.classList.remove('theme3')
+  // span.classList.add('theme1')
+  // span.classList.remove('theme2')
+  // span.classList.remove('theme3')
+
+  btnColor1.style.display='block'
+  btnColor2.style.display='none'
+  btnColor3.style.display='none'
   
 })
 themeToggle2.addEventListener('click',()=>{
   document.body.classList.add('theme2')
   document.body.classList.remove('theme1')
   document.body.classList.remove('theme3')
-  container.classList.add('theme2')
-  container.classList.remove('theme1')
-  container.classList.remove('theme3')
-  span.classList.add('theme2')
-  span.classList.remove('theme1')
-  span.classList.remove('theme3')
+  // container.classList.add('theme2')
+  // container.classList.remove('theme1')
+  // container.classList.remove('theme3')
+  // span.classList.add('theme2')
+  // span.classList.remove('theme1')
+  // span.classList.remove('theme3')
+
+  btnColor1.style.display='none'
+  btnColor2.style.display='block'
+  btnColor3.style.display='none'
+
 })
 themeToggle3.addEventListener('click',()=>{
   document.body.classList.add('theme3')
   document.body.classList.remove('theme1')
   document.body.classList.remove('theme2')
-  document.body.style.color='red'
-  container.classList.add('theme3')
-  container.classList.remove('theme1')
-  container.classList.remove('theme2')
-  span.classList.add('theme3')
-  span.classList.remove('theme1')
-  span.classList.remove('theme2')
+  // container.classList.add('theme3')
+  // container.classList.remove('theme1')
+  // container.classList.remove('theme2')
+  // span.classList.add('theme3')
+  // span.classList.remove('theme1')
+  // span.classList.remove('theme2')
+
+  btnColor1.style.display='none'
+  btnColor2.style.display='none'
+  btnColor3.style.display='block'
 })
 
 keys.forEach(key => {
